c47d32dab51bf4e6e3dd702f7b0fd083539067bf
Daniel Lungaro <chunk_77@hotmail.com>
2022-08-17 09:56:53 -0700

    Template created

---
+++ source/main.lua
@@ -0,0 +1,10 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+import "CoreLibs/crank"
+
+-- Main Game Loop
+function playdate.update()
+    playdate.graphics.drawText("Insert Game Here", 0, 0)
+end
